873c8d590bf07a9c82b27f40bf241fe01dad4551
martaP-beep <marta.ios.developer@gmail.com>
2025-03-13 19:11:20 +0100

    Sklep

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,12 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject menuPanel;
     public GameObject storePanel;
 
+    public Text magnetLevelText;
+    public Text magnetButtonText;
+
+    public Text immortalityLevelText;
+    public Text immortalityButtonText;
+
+    public Powerup magnet;
+    public Powerup immortality;
+
+    int coins;
+
+    void UpdateUI()
+    {
+        magnetLevelText.text = magnet.ToString();
+        magnetButtonText.text = magnet.UpgradeCostString();
+
+        immortalityLevelText.text = immortality.ToString();
+        immortalityButtonText.text = immortality.UpgradeCostString();
+    }
 
     public void OpenStore()
     {
@@ -38,7 +58,35 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("coins"))
+        {
+            coins = PlayerPrefs.GetInt("coins");
+        }
+        else
+        {
+            coins = 0;
+        }
+        UpdateUI();
+    }
+
+    void UpgradePowerup(Powerup powerup)
+    {
+        if(coins >= powerup.GetNextUpgradeCost()
+            && powerup.IsMaxedOut() == false)
+        {
+            powerup.Upgrade();
+            coins -= powerup.GetNextUpgradeCost();
+            PlayerPrefs.SetInt("coins", coins);
+            UpdateUI();
+        }
+    }
+    public void MagnetUpgrade()
+    {
+        UpgradePowerup(magnet);
+    }
+    public void ImmortalityUpgrade()
+    {
+        UpgradePowerup(immortality);
     }
 
     // Update is called once per frame
